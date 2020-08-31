be27d1b2b795828b37609491e8491f5c9c92706e
Leonardo Nicolas <leonicolas@gmail.com>
2020-08-30 22:07:28 -0700

    Game load method refactoring.

--- src/app/Game.js
+++ src/app/Game.js
@@ -14,15 +14,11 @@ export default class Game {
     this.buffer = buffer;
     this.context = {};
     this.control = control;
+    this.entities = new Set();
   }
 
   async load() {
-    const [tileSetImage, tileSetSpec, mapSpec, playerSpec] = await Promise.all([
-      loadImage('tile-set.png'),
-      loadSpec('tile-set.json'),
-      loadSpec('map.json'),
-      loadSpec('player.json'),
-    ]);
+    const [tileSetImage, tileSetSpec, mapSpec, playerSpec] = await this._loadDependencies();
 
     this.tileSet = new TileSet(tileSetSpec, tileSetImage);
     this.camera = new Camera(constants.VIEW_PORT_SIZE);
@@ -30,13 +26,8 @@ export default class Game {
     this.context.player = new Player(playerSpec, this.tileSet, constants.VIEW_PORT_CENTER);
     this.context.map = new GameMap(mapSpec['luna'], this.tileSet);
 
-    this.compositor = new Compositor();
-    this.compositor.addLayer(this.context.map.background());
-    this.compositor.addObject(this.context.player);
-    this.compositor.addLayer(this.context.map.foreground());
-
-    this.control.bind(this.context.player);
-    this.control.bind(this.camera);
+    this._createCompositor();
+    this._bindControl();
   }
 
   start() {
@@ -47,5 +38,26 @@ export default class Game {
       this.compositor.draw(this.buffer, this.camera);
     }
     timer.start();
+  }
+
+  async _loadDependencies() {
+    return Promise.all([
+      loadImage('tile-set.png'),
+      loadSpec('tile-set.json'),
+      loadSpec('map.json'),
+      loadSpec('player.json'),
+    ]);
+  }
+
+  _createCompositor() {
+    this.compositor = new Compositor();
+    this.compositor.addLayer(this.context.map.background());
+    this.compositor.addObject(this.context.player);
+    this.compositor.addLayer(this.context.map.foreground());
+  }
+
+  _bindControl() {
+    this.control.bind(this.context.player);
+    this.control.bind(this.camera);
   }
 }
